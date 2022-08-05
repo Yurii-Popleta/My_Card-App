80083a02f06dcc280688337d7e30b647e73844e0
Yurii Popleta <yurka279@gmail.com>
2022-08-05 15:09:03 +0200

    Initial Commit

---
+++ My_Card/My_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CardApp.swift
+//  My_Card
+//
+//  Created by Admin on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct My_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
